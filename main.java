cbf6c71a0f4cf3488d70d6806664d3cec629f138
CupheadUltra <129087076+CupheadUltra@users.noreply.github.com>
2023-08-10 23:57:16 +0300

    first cities

---
+++ main.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Main {
+  private static ArrayList<String> cities = new ArrayList<>();
+  private static Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    fillCities();
+    gameLoop();
+  }
+
+  private static void gameLoop() {
+    while (true) {
+      String input = scanner.nextLine();
+      if (input.equals("exit")) {
+        System.out.println("Дякую за гру!");
+        System.exit(0);
+      }
+      char lastChar = input.charAt(input.length() - 1);
+      for (String city : cities) {
+        char firstChar = city.charAt(0);
+        if (Character.toLowerCase(lastChar) == Character.toLowerCase(firstChar)) {
+          System.out.println("Моє місто: " + city);
+          break;
+        }
+      }
+    }
+  }
+
+  private static void fillCities() {
+    cities.add("Київ");
+    cities.add("Вінниця");
+    cities.add("Ялта");
+    cities.add("Андрушівка");
+    cities.add("Армянськ");
+    cities.add("Калуш");
+    cities.add("Шпола");
+    cities.add("Алчевськ");
+  }
+}
